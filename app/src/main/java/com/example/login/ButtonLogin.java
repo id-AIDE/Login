48c2c0c83e6688bf5928a6d114bf899c09d75613
hazan98 <hazanarnaud@gmail.com>
2016-06-04 13:16:15 +0700

    first commit

--- app/src/main/java/com/example/login/ButtonLogin.java
+++ app/src/main/java/com/example/login/ButtonLogin.java
@@ -2,31 +2,56 @@ package com.example.login;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ButtonLogin extends AppCompatActivity implements
         View.OnClickListener {
-    Button keluar;
+    Button keluar, tampilkan;
+    TextView username, password;
+String nama, pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button_login);
 
+        SharedPreferences sp = getSharedPreferences("username", MODE_PRIVATE);
+        SharedPreferences.Editor ed = sp.edit();
+
+        nama = sp.getString("nama", "");
+        pass = sp.getString("pasw", "");
+
+        username = (TextView) findViewById(R.id.user);
+                username.setText(nama);
+        password = (TextView) findViewById(R.id.pw);
+                password.setText(pass);
+
         keluar = (Button) findViewById(R.id.exitBtn);
         keluar.setOnClickListener(this);
+        tampilkan = (Button) findViewById(R.id.tampilkan);
+        tampilkan.setOnClickListener(this);
     }
     public void onClick(View clicked) {
         switch (clicked.getId()) {
             case R.id.exitBtn:
                 exit();
                 break;
+            case R.id.tampilkan:
+                tampilan();
+                break;
         }
     }
 
+    private void tampilan() {
+        Toast.makeText(getApplicationContext(), nama, Toast.LENGTH_SHORT ).show();
+        username.setText(nama);
+    }
     private void exit() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage("Apakah Kamu Benar-Benar ingin keluar?")
--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.login;
 
+import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -25,6 +27,14 @@ EditText user,pass;
         pass = (EditText)findViewById(R.id.password);
         Button submit = (Button)findViewById(R.id.ButtonLogin);
         submit.setOnClickListener(new click());
+
+
+//        SharedPreferences sp = getSharedPreferences("username", MODE_PRIVATE);
+//        String nama = sp.getString("login","");
+//        if ((nama.equals("sukses"))&&(nama!=null)){
+//            startActivity(new Intent(getApplicationContext(), ButtonLogin.class));
+//        }
+
     }
     class click implements Button.OnClickListener{
 
@@ -36,6 +46,13 @@ EditText user,pass;
             {
                 Toast.makeText(getApplicationContext(),"selamat datang", Toast.LENGTH_LONG).show();
                 startActivity(new Intent(getApplicationContext(), ButtonLogin.class));
+
+                SharedPreferences sp = getSharedPreferences("username", MODE_PRIVATE);
+                SharedPreferences.Editor ed = sp.edit();
+                ed.putString("nama", nama);
+                ed.putString("pasw", pasw);
+                ed.commit();
+
             }
             else
                 Toast.makeText(getApplicationContext(),"login anda salah", Toast.LENGTH_SHORT).show();
